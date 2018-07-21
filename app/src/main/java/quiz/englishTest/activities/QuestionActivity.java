8f7ed5ed70f8c49750e3e1cdca53052660ccf459
Hajni <berecz.hajnalka@gmail.com>
2018-07-21 17:23:03 +0200

    Create inner class for DialogFragment

--- app/src/main/java/quiz/englishTest/activities/QuestionActivity.java
+++ app/src/main/java/quiz/englishTest/activities/QuestionActivity.java
@@ -1,5 +1,7 @@
 package quiz.englishTest.activities;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioAttributes;
@@ -9,6 +11,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -21,9 +26,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import quiz.englishTest.R;
 import quiz.englishTest.data.AnswerContract;
 import quiz.englishTest.data.WordsContract;
-import quiz.englishTest.R;
 import quiz.englishTest.model.Answer;
 import quiz.englishTest.model.Word;
 
@@ -303,6 +308,27 @@ public class QuestionActivity extends AppCompatActivity {
         super.onPause();
         isInBackground = true;
     }
+
+    public static class ExitTestDialogFragment extends DialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.confirm_exit)
+                    .setPositiveButton(R.string.exit, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            getActivity().finish();
+                        }
+                    })
+                    .setNegativeButton(R.string.stay, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                        }
+                    });
+            return builder.create();
+        }
+
+    }
+
 
 }
 
